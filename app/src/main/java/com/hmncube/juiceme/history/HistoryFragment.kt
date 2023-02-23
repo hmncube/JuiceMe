8fa13bf8b8922bb1b2b7e6ba8aef04534959b88f
happinessncube <happinessmncube@gmail.com>
2023-02-23 16:08:25 +0200

    Added copy function

--- app/src/main/java/com/hmncube/juiceme/history/HistoryFragment.kt
+++ app/src/main/java/com/hmncube/juiceme/history/HistoryFragment.kt
@@ -1,5 +1,9 @@
 package com.hmncube.juiceme.history
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
@@ -16,6 +20,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hmncube.juiceme.R
+import com.hmncube.juiceme.UserFeedback
 import com.hmncube.juiceme.ViewModelFactory
 import com.hmncube.juiceme.data.AppDatabase
 import com.hmncube.juiceme.data.CardNumber
@@ -94,6 +99,22 @@ class HistoryFragment : Fragment(), OptionsMenuClickListener {
                 HomeFragment.dialNumber(codePrefix, cardNumber.number, viewBinding.root, requireContext())
                 return true
             }
+            R.id.optionMenuCopy -> {
+                val clipboard = requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val clip: ClipData =
+                    ClipData.newPlainText(
+                        requireContext().resources.getString(R.string.recharge_code), cardNumber.number
+                    )
+                clipboard.setPrimaryClip(clip)
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2) {
+                    UserFeedback.displayFeedback(
+                        requireView(), String.format(
+                            resources.getString(R.string.copied_recharge_code), cardNumber.number),
+                        UserFeedback.LENGTH_SHORT
+                    )
+                }
+                return true
+            }
         }
         return false
     }
@@ -102,7 +123,7 @@ class HistoryFragment : Fragment(), OptionsMenuClickListener {
         val alertDialog: AlertDialog? = activity?.let {
             val builder = AlertDialog.Builder(it)
             builder.apply {
-                setPositiveButton(R.string.delete) { _, _ ->
+                setPositiveButton(R.string.menu_delete) { _, _ ->
                     viewModel.deleteEntry(cardNumber)
                     adapter.deletedItem(position)
                 }
